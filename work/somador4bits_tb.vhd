9acc8c36bae95fb18fa3822f409fd70d4a670f55
breno-cn <38513013+breno-cn@users.noreply.github.com>
2018-05-16 14:13:53 -0300

    Update somador4bits_tb.vhd

--- work/somador4bits_tb.vhd
+++ work/somador4bits_tb.vhd
@@ -9,16 +9,16 @@ architecture TB of somador4bits_tb is
 
 component somador4bits is
   port(
-    opa : IN std_logic_vector(3 downto 0);
-    opb : IN std_logic_vector(3 downto 0);
-    ops : OUT std_logic_vector(3 downto 0);
+    opa    : IN std_logic_vector(3 downto 0);
+    opb    : IN std_logic_vector(3 downto 0);
+    ops    : OUT std_logic_vector(3 downto 0);
     opcout : OUT std_logic
     );
   end component;
   
-  signal A, B :         std_logic_vector(3 downto 0);
-  signal carry:         std_logic;
-  signal S    :         std_logic_vector(3 downto 0);
+  signal A, B  :         std_logic_vector(3 downto 0);
+  signal carry :         std_logic;
+  signal S     :         std_logic_vector(3 downto 0);
   
   begin
     UUT_s4bits: somador4bits port map (A, B, S, carry);
@@ -30,11 +30,11 @@ component somador4bits is
       B <= "0000";
       Wait for 10 ns;
       assert (s="0000") report "Error!" severity error;
-      assert (carry='0')report "Carry Error!" severity error;
+      assert (carry='0') report "Carry Error!" severity error;
       
       --case 2
       A <= "1111";
-      B<= "0001";
+      B <= "0001";
       wait for 10 ns;
       assert (s="0000") report "Sum Error!" severity error;
       assert (carry='1') report "Carry Error!" severity error;
