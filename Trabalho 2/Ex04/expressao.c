6a32823deb828c3ca63c64389f34e51c60c2bc77
breno-cn <38513013+breno-cn@users.noreply.github.com>
2018-07-09 12:36:29 -0300

    Add files via upload

--- Trabalho 2/Ex04/expressao.c
+++ Trabalho 2/Ex04/expressao.c
@@ -56,39 +56,58 @@ int infixa_posfixa(const char *inf, char *pos){
 	return 1;
 }
 
+int infixa_posfixa2(const char *inf, char *pos){
+	if(inf==NULL || pos==NULL) return 0;
+	int j=0,x;
+	pilha p=cria_pilha();
+	for(int i=0;inf[i]!='\0';i++){
+		if(inf[i]>='A' || inf[i]<='B') push(&p,inf[i]);
+	}
+	for(int i=0;inf[i]!='\0';i++){
+		if(inf[i]=='+' || inf[i]=='-' || inf[i]=='*'
+ 		 || inf[i]=='/' || inf[i]=='^' || inf[i]=='('){
+			  pop(&p,&x);
+			  pos[j++]=x;
+		  }
+	}
+	while(!pilha_vazia(p)){
+		pop(&p,&x);
+		pos[j++]=x;
+	}
+	pos[j]='\0';
+	return 1;
+}
+
 int resultado_pos(const char *s, int *lit){
 	if(s==NULL || lit==NULL) return 0;
-	int x=0, num1, num2, op, flag=0;
+	int x=0, num1, num2, op;
 	pilha p=cria_pilha();
 	fila sinal=cria_fila();
 	for(int i=0;s[i]!='\0';i++){
-		if(s[i]>='A' && s[i]<='J') push(&p,lit[s[i]-'A']);
-		if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/'
-		 || s[i]=='^'){
+		if(s[i]>='A' && s[i]<='J'){
+			push(&p,lit[s[i]-'A']);
+			printf("Literal %c vale: %d\n", s[i], lit[s[i]-'A']);
+		}
+		if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/' || s[i]=='^'){
 			insere_fim(sinal,s[i]);
 			}
 	}
+	remove_ini(sinal,&op);
+	pop(&p,&num2);
+	pop(&p,&num1);
+	if(op=='+') x=num1+num2;
+	if(op=='-') x=num1-num2;
+	if(op=='*') x=num1*num2;
+	if(op=='/') x=num1/num2;
+	if(op=='^') x=pow(num1,num2);
 	while(!fila_vazia(sinal)){
-		if(!flag){
-			remove_ini(sinal, &op);
-			pop(&p,&num2);
-			pop(&p,&num1);
-			if(op=='+') x+=num1+num2;
-			if(op=='-') x+=num1-num2;
-			if(op=='*') x+=num1*num2;
-			if(op=='/') x+=num1/num2;
-			if(op=='^') x+=pow(num1,num2);
-			flag=1;
-		}
-		if(flag){
-			remove_ini(sinal,&op);
-			pop(&p,&num1);
-			if(op=='+') x+=num1;
-			if(op=='-') x-=num1;
-			if(op=='*') x*=num1;
-			if(op=='/') x/=num1;
-			if(op=='^') x=pow(num1,x);
-		}
+		remove_ini(sinal,&op);
+		pop(&p,&num1);
+		if(op=='+') x+=num1;
+		if(op=='-') x = (x>num1) ? num1-x : x-num1;
+		if(op=='*') x*=num1;
+		if(op=='/') x/=num1;
+		if(op=='^') x=pow(x,num1);
 	}
 	return x;
 }
